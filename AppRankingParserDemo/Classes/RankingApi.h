d3fedbe73bc52a4577eed636280da169b182fa02
SOMTD <somtd1104@me.com>
2013-08-16 11:09:12 +0900

    Adding AFNetworking

---
+++ AppRankingParserDemo/Classes/RankingApi.h
@@ -0,0 +1,13 @@
+//
+//  RankingApi.h
+//  AppRankingParserDemo
+//
+//  Created by SOMTD on 2013/08/16.
+//  Copyright (c) 2013年 SOMTD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RankingApi : NSObject
+
+@end
---
+++ AppRankingParserDemo/Classes/RankingApi.m
@@ -0,0 +1,13 @@
+//
+//  RankingApi.m
+//  AppRankingParserDemo
+//
+//  Created by SOMTD on 2013/08/16.
+//  Copyright (c) 2013年 SOMTD. All rights reserved.
+//
+
+#import "RankingApi.h"
+
+@implementation RankingApi
+
+@end
